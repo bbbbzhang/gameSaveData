ccf8f1ab2943cf049d73916ccb5d0b33d7b49bdf
bbbbzhang <your_email@example.com>
2025-09-08 18:48:24 +0800

    游戏存档

---
+++ Users/19654/AppData/Roaming/Gravity Circuit/userconfig/playerSettings.lua
@@ -0,0 +1,44 @@
+-- Global Player settings configuration file for Gravity Circuit.
+-- This file follows usual lua rules, so if you wish to edit it manually, follow the structure
+-- of the variables laid out here. Any changes you do are to be done at your own risk and,
+-- in some cases, may not result in any changes anyway. Too bad!
+return {
+	DISPLAY=1,
+	RESOLUTION_MULTIPLIER=2,
+	FULLSCREEN=true,
+	VSYNC=false,
+	FORCE_ASPECT_RATIO=true,
+	STRETCH_FULLSCREEN=false,
+	GRAPHICS_SMOOTHING=1,
+	ANISOTROPY=0,
+	LANGUAGE=7,
+	EDITOR={
+		SCROLL_MOUSE=true,
+		SCROLL_KEYBOARD=true,
+		SCROLL_SPEED_KB=4,
+		SCROLL_SPEED_MOUSE=2,
+		MINIMAP_OPEN_BY_DEFAULT=true,
+	},
+	VOLUME={
+		SFX=1,
+		BGM=1,
+		MASTER=0.55,
+	},
+	DIALOGUE_SPEED=1,
+	BORDERLESS_WINDOW=false,
+	EFFECT_TIMERS=true,
+	FSAA=0,
+	CHALLENGE_NOTIFICATIONS=true,
+	GAME_HEIGHT=240,
+	SPEEDRUN_TIMER_SETTING=1,
+	FIXED_FRAMERATE=2,
+	SHOW_BURST_CHARGES=1,
+	EFFECTS={
+		GIBS_HAVE_PHYSICS=true,
+		GIBS_SPAWN=true,
+	},
+	GAME_WIDTH=400,
+	FULLSCREEN_TYPE=1,
+	LANGUAGE_LIST='-en-fr-de-es-nl-jp-chs-cht',
+	BUILD_VERSION='VERSION: 1.1.1a',
+}
